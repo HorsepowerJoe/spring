3a5a109406dc96a240e3cdd79194d311847cb642
JML <skadidrhrl@gmail.com>
2023-08-30 20:43:41 +0900

    DDL 수정

--- src/main/java/com/toyproject/spring/CommandLineRunner/DummyDataLoader.java
+++ src/main/java/com/toyproject/spring/CommandLineRunner/DummyDataLoader.java
@@ -19,15 +19,20 @@ public class DummyDataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Grooming grooming = new Grooming();
         Grooming grooming2 = new Grooming();
+        Grooming grooming3 = new Grooming();
 
-        grooming.setG_pricePerWeight(new BigInteger("10000"));
+        grooming.setG_pricePerWeight(new BigInteger("20000"));
         grooming.setG_styleName("스포팅");
 
         grooming2.setG_pricePerWeight(new BigInteger("30000"));
         grooming2.setG_styleName("가위컷");
 
+        grooming3.setG_pricePerWeight(new BigInteger("15000"));
+        grooming3.setG_styleName("위생미용");
+
         groomingRepository.save(grooming);
         groomingRepository.save(grooming2);
+        groomingRepository.save(grooming3);
 
     }
 
