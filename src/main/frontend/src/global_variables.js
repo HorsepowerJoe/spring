33d59fd902da6a2a58eac683dd0ae1538c056a63
HorsepowerJoe <124421204+HorsepowerJoe@users.noreply.github.com>
2023-09-14 17:40:17 +0900

    Delete src/main/frontend/src/global_variables.js
    sec

--- src/main/frontend/src/global_variables.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_KEY =
-  "627365337071-s8qibo5oh3u0p992qv54asg38qbcopsf.apps.googleusercontent.com";
-export const FACEBOOK_KEY = "1208850299781593";
-export const NAVER_KEY = "b87_UJySEzVAsvplRX0N";
